fe4f80487f7e94950cc4a245e33eb427291ff2a6
RokuroSatp <a-cclemon-mesopota-root36@live.jp>
2018-03-23 16:50:48 +0900

    [FIX]select target channel

--- src/rokudiscobot_prototype.py
+++ src/rokudiscobot_prototype.py
@@ -1,5 +1,7 @@
 from util.configloader import Config
 import discord
+import re
+import sys
 
 def main():
     conf = Config.load()
@@ -11,28 +13,67 @@ def main():
         print(client.user.name)
         print(client.user.id)
         print('------')
-    
+    import traceback
+
+    def log_message_information(message: discord.Message):
+        try:
+            print(message.channel.name)
+            print(message.channel.is_private)
+            if message.channel.is_private:
+                print(message.channel.owner)
+                print(message.channel.recipients)
+            else:
+                print(dir(message))
+            print(message.channel_mentions)
+            print(message.mentions)
+            print(message.mention_everyone)
+        except:
+            exc_type, exc_val, tb = sys.exc_info()
+            traceback.print_exception(exc_type, exc_val, tb)
+        """dir(Channel)
+        [..., '_clean_content', '_handle_call', '_handle_mentions',
+        '_handle_upgrades', '_raw_channel_mentions', '_raw_mentions', '_raw_role_mentions',
+        '_system_content', '_update',
+        'attachments', 'author', 'call', 'channel', 'channel_mentions', 'clean_content',
+        'content', 'edited_timestamp', 'embeds', 'id', 'mention_everyone', 'mentions',
+        'nonce', 'pinned', 'raw_channel_mentions', 'raw_mentions', 'raw_role_mentions', 'reactions',
+        'role_mentions', 'server', 'system_content', 'timestamp', 'tts', 'type']
+        """
+        """dir(PrivateChannel)
+        [..., '_update_group',
+        'created_at', 'icon', 'icon_url', 'id', 'is_private',
+        'me', 'name', 'owner', 'permissions_for', 'recipients', 'type', 'user']
+        """
+
     @client.event
     async def on_message(message: discord.Message):
-        if client.user != message.author and message.is_private:
-            # 「おはよう」で始まるか調べる
-            if message.content.startswith("おはよう") or message.content.startswith("Hi") or message.content.startswith("Hello"):
-                # 送り主がBotだった場合反応したくないので
-                    # メッセージを書きます
+        main_channel = "bot"
+        active_channels = ["dev", "bot", "Direct Message with rokusan63"]
+        if client.user != message.author:
+            log_message_information(message)
+            if message.channel.is_private or message.channel.name in active_channels:
+                # 「おはよう」で始まるか調べる
+                if re.match(r"^(おはよう|よお|よう|Hello|はろー)", message.content):
                     m = "おはようございます" + message.author.name + "さん！"
                     # メッセージが送られてきたチャンネルへメッセージを送ります
                     await client.send_message(message.channel, m)
-            elif "yo" in message.content.lower():
-                m = "yo"
-                # メッセージが送られてきたチャンネルへメッセージを送ります
-                await client.send_message(message.channel, m)
-            else:
-                m = "わかる"
-                # メッセージが送られてきたチャンネルへメッセージを送ります
-                await client.send_message(message.channel, m)
-                
-        
-    client.run(conf.discord_apikey)
+                elif re.match(r"^わかり手だ", message.content):
+                    m = "そうだよ"
+                    # メッセージが送られてきたチャンネルへメッセージを送ります
+                    await client.send_message(message.channel, m)
+                elif re.match(r"^わかる", message.content):
+                    m = "わかり手だ"
+                    # メッセージが送られてきたチャンネルへメッセージを送ります
+                    await client.send_message(message.channel, m)
+                elif "yo" in message.content.lower():
+                    m = "yo"
+                    # メッセージが送られてきたチャンネルへメッセージを送ります
+                    await client.send_message(message.channel, m)
+    try:
+        client.run(conf.discord_apikey)
+    finally:
+        print("client exit")
+        client.close()
 
 if __name__ == "__main__":
     main()
